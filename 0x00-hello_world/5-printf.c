51451e552f96e7ee7ab53274257b740dae955de4
Marinaessam <marinaesaam@gmail.com>
2023-04-20 07:47:06 -0700

    Modify 1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,12 +5,12 @@
  *
  * Description: prinf f with proper grammar, but the outcome is a piece of art,
  *
- * return =0
+ * return = 0
  *
  */
 
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art, \n");
 	return (0);
 }
