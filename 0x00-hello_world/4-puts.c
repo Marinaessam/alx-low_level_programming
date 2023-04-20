8cac18dbb90760e413006d31e498dc15cc419ade
Marinaessam <marinaesaam@gmail.com>
2023-04-20 08:47:43 -0700

    Modify

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,12 @@
 /**
  * main - Entry point
  * Description prints Programming is like building a multilingual puzzle
- * Return value=0;
+ * return value of 'main'=0
 */
 
 int main(void)
-{	puts("\"Programming is like building a multilingual puzzle");
+{	
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);     
 } 
 
