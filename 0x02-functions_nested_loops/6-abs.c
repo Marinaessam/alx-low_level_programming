ec4399f73a9da0273d48bc9fcd4ed9273e2719d3
Marinaessam <marinaesaam@gmail.com>
2023-04-25 12:04:52 -0700

    C

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+* _abs - value of an integer
+* integar n:  is an integer
+* Return: integer
+*/
+
+int _abs(int);
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
