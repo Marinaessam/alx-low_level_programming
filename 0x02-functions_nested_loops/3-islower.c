04f1710df6301467b28785b9e596d9ee17d93cc0
Marinaessam <marinaesaam@gmail.com>
2023-04-25 10:30:20 -0700

    C

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,13 +8,12 @@
 * Return: 1 if true. 0 if false.
 */
 
-int _islower(int c);
+int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
-
 	else
 	{
 		return (0);
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,7 +8,7 @@
 * Return: 1 if n is greater & less than zero, else return 0
 */
 
-int print_sign(int n);
+int print_sign(int n)
 {
 	if (n > 0)
 	{
