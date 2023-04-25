82ab24dfdaea04a89997b769281bb7bd9ff6c6bd
Marinaessam <marinaesaam@gmail.com>
2023-04-25 08:48:23 -0700

    C

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,16 +7,15 @@
 * Return: void
 */
 
-void print_alphabet(void);
+
+void print_alphabet(void)
 {
-    	char letter = 'a';
+	char letter = 'a';
 
-        while (letter <= 'z')
-
-        {
-                _putchar (letter);
-                letter++
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
 	}
 	_putchar('\n');
-
 }
