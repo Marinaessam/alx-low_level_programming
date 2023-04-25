b7e7ebede5888dca54b6a2d4bdcb7dbe42b7c944
Marinaessam <marinaesaam@gmail.com>
2023-04-25 08:11:44 -0700

    C Functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+* main - Entry Point
+* Description - prints the alphabet, in lowercase, followed by a new line.
+* Return: 0
+*/
+
+void print_alphabet(void);
+{
+	char letter = 'a'
+
+	while (letter <= 'z')
+
+	{
+		_putchar (letter);
+		letter++
+	}
+
+	return (0);
+}
