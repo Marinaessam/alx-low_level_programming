35c02629eb38f19cdffc5829a64d79d7144bd918
Marinaessam <marinaesaam@gmail.com>
2023-05-01 23:31:20 -0700

    Pointer & Arrays

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the value of two integers.
+ * @a: first integer.
+ * @b: second integer.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int j;
+
+	j = 0;
+	j = *a;
+	*a = *b;
+	*b = c;
+}
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print elements of array.
+ * @a: int of array to print.
+ * @n: numbers to print.
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
